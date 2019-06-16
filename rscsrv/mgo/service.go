d381be92176f0777f2e5236b39005769026f67b6
Felipe Pontes <felipemfpontes@gmail.com>
2019-06-16 11:11:38 -0300

    Add default MgoService implementation using `athena/config`

---
+++ rscsrv/mgo/service.go
@@ -0,0 +1,33 @@
+package rscsrv
+
+import (
+	"github.com/globalsign/mgo"
+
+	"github.com/lab259/athena/config"
+	mgosrv "github.com/lab259/go-rscsrv-mgo"
+)
+
+type MgoService struct {
+	mgosrv.MgoService
+}
+
+func (service *MgoService) Name() string {
+	return "Mongo Service"
+}
+
+var DefaultMgoService MgoService
+
+func (service *MgoService) LoadConfiguration() (interface{}, error) {
+	var configuration mgosrv.MgoServiceConfiguration
+	err := config.Load("mgo-main.yml", &configuration)
+	if err != nil {
+		return nil, err
+	}
+	return configuration, nil
+}
+
+func (service *MgoService) RunWithDB(handler func(db *mgo.Database) error) error {
+	return service.RunWithSession(func(session *mgo.Session) error {
+		return handler(session.DB(service.Configuration.Database))
+	})
+}
