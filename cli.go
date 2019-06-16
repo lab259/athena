ce5c7315ae90207c55a4874ef871f059577e47f9
Felipe Pontes <felipemfpontes@gmail.com>
2019-06-16 08:34:04 -0300

    Add ability to override bind address, default wait and hostname
    Refactor help and version options to use built-in capabilities

--- cli.go
+++ cli.go
@@ -3,6 +3,7 @@ package athena
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	cli "github.com/jawher/mow.cli"
@@ -13,10 +14,8 @@ import (
 type CLIOptions struct {
 	BindAddress *string
 	Wait        *int
-	HostName    *string
+	Hostname    *string
 	isDryRun    *bool
-	isHelp      *bool
-	isVersion   *bool
 }
 
 type cliBuilder struct {
@@ -24,12 +23,19 @@ type cliBuilder struct {
 	env            string
 	version        string
 	build          string
+	wait           int
+	bindAddress    string
+	hostname       string
 	serviceStarter *rscsrv.ServiceStarter
 }
 
 func NewCLI(name, description string) *cliBuilder {
+	hostname, _ := os.Hostname()
 	return &cliBuilder{
-		cli: cli.App(name, description),
+		cli:         cli.App(name, description),
+		bindAddress: "127.0.0.1:3000",
+		wait:        0,
+		hostname:    hostname,
 	}
 }
 
@@ -47,7 +53,21 @@ func (b *cliBuilder) Environment(env string) *cliBuilder {
 func (b *cliBuilder) ServiceStarter(serviceStarter *rscsrv.ServiceStarter) *cliBuilder {
 	b.serviceStarter = serviceStarter
 	return b
+}
 
+func (b *cliBuilder) BindAddress(bindAddress string) *cliBuilder {
+	b.bindAddress = bindAddress
+	return b
+}
+
+func (b *cliBuilder) Wait(wait int) *cliBuilder {
+	b.wait = wait
+	return b
+}
+
+func (b *cliBuilder) Hostname(hostname string) *cliBuilder {
+	b.hostname = hostname
+	return b
 }
 
 func (b *cliBuilder) Build() (*cli.Cli, *CLIOptions) {
@@ -55,51 +75,42 @@ func (b *cliBuilder) Build() (*cli.Cli, *CLIOptions) {
 
 	options.BindAddress = b.cli.String(cli.StringOpt{
 		Name:   "B bind-address",
-		Value:  "127.0.0.1:3000",
+		Value:  b.bindAddress,
 		Desc:   "The bind address will be used on the HTTP server",
 		EnvVar: "BIND_ADDR",
 	})
 
 	options.Wait = b.cli.Int(cli.IntOpt{
 		Name:   "w wait",
-		Value:  0,
+		Value:  b.wait,
 		Desc:   "Delay in seconds before the initialization",
 		EnvVar: "WAIT",
 	})
 
-	hostName, _ := os.Hostname()
-	options.HostName = b.cli.String(cli.StringOpt{
+	options.Hostname = b.cli.String(cli.StringOpt{
 		Name:   "H hostname",
-		Value:  hostName,
+		Value:  b.hostname,
 		Desc:   "The name of the station running the app instance",
 		EnvVar: "HOSTNAME",
 	})
-	os.Setenv("HOSTNAME", *options.HostName)
+	os.Setenv("HOSTNAME", *options.Hostname)
 
 	options.isDryRun = b.cli.BoolOpt("d dry-run", false, "Loads the configuration and check if the dependencies are working (such as database connections)")
-	options.isHelp = b.cli.BoolOpt("h help", false, "Displays this help message")
-	options.isVersion = b.cli.BoolOpt("v version", false, "Displays the version")
+
+	var version strings.Builder
+	version.WriteString(fmt.Sprintf("Version: %s", b.version))
+	if b.build != "" {
+		version.WriteString(fmt.Sprintf("\n  Build: %s", b.build))
+	}
+	b.cli.Version("v version", version.String())
 
 	b.cli.Before = func() {
-		if *options.isHelp {
-			b.cli.PrintLongHelp()
-			os.Exit(0)
-		}
-
-		if *options.isVersion {
-			fmt.Printf("Version: %s\n", b.version)
-			if b.build != "" {
-				fmt.Printf("  Build: %s\n", b.build)
-			}
-			os.Exit(0)
-		}
-
 		if *options.isDryRun {
-			rlog.Info("Dry run!")
+			rlog.Trace(1, "This is a dry run!")
 		}
 
 		rlog.Infof("Version: %s (%s)", b.version, b.build)
-		rlog.Infof("Loading configuration from '%s' ...", b.env)
+		rlog.Infof("Environment: %s", b.env)
 
 		if *options.Wait > 0 {
 			rlog.Infof("  Waiting %d seconds before continue ...", *options.Wait)
@@ -123,6 +134,10 @@ func (b *cliBuilder) Build() (*cli.Cli, *CLIOptions) {
 					rlog.Critical(err)
 					os.Exit(2)
 				}
+			}
+
+			if *options.isDryRun {
+				rlog.Trace(1, "Everything looks fine!")
 			}
 			os.Exit(0)
 		}
