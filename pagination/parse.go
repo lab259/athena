07e4ee1cd1047ccf5f342a32e969c7978ac8efd0
Felipe Pontes <felipemfpontes@gmail.com>
2019-06-18 10:34:35 -0300

    Add ability to customize Pagination defaults

--- pagination/parse.go
+++ pagination/parse.go
@@ -2,21 +2,51 @@ package pagination
 
 import "math"
 
-const (
-	DefaultPageSize = 10
-	MaxPageSize     = 100
+var (
+	defaultPageSize = 10
+	maxPageSize     = 100
 )
 
-// Parse adjust pageSize and currentPage to respect `MaxPageSize`.
-//
-// `currentPage` starts at 1.
+// Parse adjust pageSize and currentPage to respect default page size
+// and max page size. Keep in mind that the first page is `1`, therefore non-positive
+// numbers will always return `1`.
 func Parse(pageSizeRaw, currentPageRaw int) (pageSize int, currentPage int) {
-	pageSize = int(math.Min(float64(pageSizeRaw), float64(MaxPageSize)))
+	pageSize = int(math.Min(float64(pageSizeRaw), float64(maxPageSize)))
 	if pageSize <= 0 {
-		pageSize = DefaultPageSize
+		pageSize = defaultPageSize
 	}
 
 	currentPage = int(math.Max(1, float64(currentPageRaw)))
+	return
+}
 
-	return pageSize, currentPage
+// Option represents functions that can be used to customize
+// pagination defaults.
+//
+//
+// See Also
+//
+// pagination.Setup
+type Option func()
+
+// MaxPageSize customize max page size.
+func MaxPageSize(n int) Option {
+	return func() {
+		maxPageSize = n
+	}
+}
+
+// DefaultPageSize customize default page size.
+func DefaultPageSize(n int) Option {
+	return func() {
+		defaultPageSize = n
+	}
+}
+
+// Setup allows customize default values:
+//  pagination.Setup(pagination.DefaultPageSize(20), pagination.MaxPageSize(50))
+func Setup(options ...Option) {
+	for _, opt := range options {
+		opt()
+	}
 }
