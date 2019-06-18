b302a3cd049cddd35764010259c126bdd6f78d23
Felipe Pontes <felipemfpontes@gmail.com>
2019-06-18 12:45:39 -0300

    Refactor commands to output relative path to files

--- athena/make/service.go
+++ athena/make/service.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 
 	"github.com/iancoleman/strcase"
 	cli "github.com/jawher/mow.cli"
@@ -24,6 +25,9 @@ func Service(cmd *cli.Cmd) {
 	cmd.StringsArgPtr(&data.Fields, "INPUT_FIELD", []string{}, "input field (eg.: Name:string)")
 
 	cmd.Action = func() {
+		createdFiles := make([]string, 0, 3)
+		defer dumpFiles(&createdFiles)
+
 		data.Service = strcase.ToCamel(data.Service)
 		data.Package = strcase.ToSnake(data.Package)
 
@@ -37,15 +41,33 @@ func Service(cmd *cli.Cmd) {
 
 		serviceFile := fmt.Sprintf("%s.go", path.Join(packageDir, strcase.ToSnake(data.Service)))
 		serviceTestFile := fmt.Sprintf("%s_test.go", path.Join(packageDir, strcase.ToSnake(data.Service)))
+		packageTestFile := fmt.Sprintf("%s_test.go", path.Join(packageDir, data.Package))
 
 		err = template.Write(templates.ServiceTemplate, &data, serviceFile)
 		util.HandleError(err, "Unable to create service.")
+		createdFiles = append(createdFiles, serviceFile)
 
 		err = template.Write(templates.ServiceTestTemplate, &data, serviceTestFile)
 		util.HandleError(err, "Unable to create service tests.")
+		createdFiles = append(createdFiles, serviceTestFile)
 
-		fmt.Println("The following files were created:")
-		fmt.Printf("  %s\n", serviceFile)
-		fmt.Printf("  %s\n", serviceTestFile)
+		if _, err := os.Stat(packageTestFile); os.IsNotExist(err) {
+			err = template.Write(templates.ServiceTestsTemplate, &data, packageTestFile)
+			util.HandleError(err, "Unable to create package tests.")
+			createdFiles = append(createdFiles, packageTestFile)
+		}
+	}
+}
+
+func dumpFiles(files *[]string) {
+	fmt.Println("The following files were created:")
+
+	dir, err := os.Getwd()
+	for _, file := range *files {
+		if err != nil {
+			fmt.Printf("  %s\n", file)
+		} else {
+			fmt.Printf("  %s\n", strings.Replace(file, dir, ".", 1))
+		}
 	}
 }
