169d472329fe20047a4a25d286b2f8576d1da7cb
Felipe Pontes <felipemfpontes@gmail.com>
2019-06-18 12:27:36 -0300

    Add Ginkgo testing utils

---
+++ testing/env/override.go
@@ -0,0 +1,16 @@
+package env
+
+import "os"
+
+func Override(vars map[string]string, callback func() error) error {
+	current := make(map[string]string)
+	for k, v := range vars {
+		current[k] = os.Getenv(k)
+		os.Setenv(k, v)
+	}
+	err := callback()
+	for k, v := range current {
+		os.Setenv(k, v)
+	}
+	return err
+}
---
+++ testing/ginkgo/init.go
@@ -0,0 +1,48 @@
+package ginkgo
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gosimple/slug"
+	"github.com/jamillosantos/macchiato"
+	"github.com/lab259/athena/config"
+	"github.com/lab259/athena/testing/env"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
+	"github.com/onsi/gomega"
+)
+
+func Init(description string, t *testing.T) {
+	env.Override(map[string]string{
+		"PROJECT_ROOT": config.ProjectRoot(),
+	}, func() error {
+		if os.Getenv("ENV") == "" {
+			err := os.Setenv("ENV", "test")
+			if err != nil {
+				panic(err)
+			}
+		}
+		dir, _ := os.Getwd()
+		ginkgo.GinkgoWriter.Write([]byte(fmt.Sprintf("CWD: %s\n", dir)))
+		ginkgo.GinkgoWriter.Write([]byte(fmt.Sprintf("Starting with ENV: %s\n", os.Getenv("ENV"))))
+		gomega.RegisterFailHandler(ginkgo.Fail)
+
+		if os.Getenv("CI") == "" {
+			macchiato.RunSpecs(t, description)
+		} else {
+			reporterOutputDir := fmt.Sprintf("%s/test-results", config.ProjectRoot())
+			os.RemoveAll(reporterOutputDir)
+			os.MkdirAll(reporterOutputDir, os.ModePerm)
+			junitReporter := reporters.NewJUnitReporter(fmt.Sprintf("%s/%d_%s.xml", reporterOutputDir, time.Now().Unix(), slug.Make(description)))
+			macchiatoReporter := macchiato.NewReporter()
+			ginkgo.RunSpecsWithCustomReporters(t, description, []ginkgo.Reporter{
+				macchiatoReporter,
+				junitReporter,
+			})
+		}
+		return nil
+	})
+}
