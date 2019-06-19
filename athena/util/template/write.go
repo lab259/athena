72d7e9e4e0d73f594f5a90d5812354ecb76b4987
Felipe Pontes <felipemfpontes@gmail.com>
2019-06-19 16:56:34 -0300

    Refactor to format generate files

--- athena/util/template/write.go
+++ athena/util/template/write.go
@@ -2,15 +2,20 @@ package template
 
 import (
 	"bytes"
+	"go/format"
 	"io/ioutil"
 	"text/template"
 )
 
 func Write(tmpl *template.Template, data interface{}, name string) error {
-	content := bytes.NewBuffer([]byte{})
-	err := tmpl.Execute(content, data)
+	var content bytes.Buffer
+	err := tmpl.Execute(&content, data)
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(name, content.Bytes(), 0644)
+	formatted, err := format.Source(content.Bytes())
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(name, formatted, 0644)
 }
