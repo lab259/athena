bd6ff0799c5e340f362adbfd1395f0d529699be0
Felipe Pontes <felipemfpontes@gmail.com>
2019-08-16 13:00:19 -0300

    Add ability to set Before/After hooks on Setup (#4)
    Useful for running BeforeEach/AfterEach functions without worrying whether the services are running or not.

---
+++ testing/rscsrvtest/setup_test.go
@@ -0,0 +1,59 @@
+package rscsrvtest_test
+
+import (
+	"time"
+
+	"github.com/lab259/athena/testing/rscsrvtest"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type clockService struct {
+	startedAt time.Time
+	stoppedAt time.Time
+}
+
+func (s *clockService) Name() string {
+	panic("not implemented")
+}
+
+func (s *clockService) Restart() error {
+	if err := s.Stop(); err != nil {
+		return err
+	}
+	return s.Start()
+}
+
+func (s *clockService) Start() error {
+	s.startedAt = time.Now()
+	time.Sleep(time.Millisecond)
+	return nil
+}
+
+func (s *clockService) Stop() error {
+	s.stoppedAt = time.Now()
+	time.Sleep(time.Millisecond)
+	return nil
+}
+
+var _ = Describe("ClockServiceTest", func() {
+	var serv clockService
+	var beforeCalled time.Time
+	var afterCalled time.Time
+
+	rscsrvtest.Before(func() {
+		beforeCalled = time.Now()
+		time.Sleep(time.Millisecond)
+	}).After(func() {
+		afterCalled = time.Now()
+		time.Sleep(time.Millisecond)
+	}).Setup(&serv)
+
+	It("should run before hook after services are started", func() {
+		Expect(beforeCalled).To(BeTemporally(">", serv.startedAt))
+	})
+
+	It("should run after hook before services are stopped", func() {
+		Expect(afterCalled).To(BeTemporally("<", serv.stoppedAt))
+	})
+})
