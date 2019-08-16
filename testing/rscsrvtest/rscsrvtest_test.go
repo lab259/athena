30ccc08ae48d25c22d4cb3934ea6d5a865f75daf
Felipe Pontes <felipemfpontes@gmail.com>
2019-08-16 12:42:52 -0300

    Add tests for Before/After hooks

---
+++ testing/rscsrvtest/rscsrvtest_test.go
@@ -0,0 +1,11 @@
+package rscsrvtest_test
+
+import (
+	"testing"
+
+	"github.com/lab259/athena/testing/ginkgotest"
+)
+
+func TestRscsrvTestService(t *testing.T) {
+	ginkgotest.Init("athena/testing/rscsrvtest Test Suite", t)
+}
